d479478ee96f7f3bc9f689c28de5679d7eb78d02
krishpatel007a-ctrl <krishpatel007a@gmail.com>
2026-05-27 09:17:01 +0530

    express

--- t7/index.js
+++ t7/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
+});
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to Home page!</h1>');
+});
+
+app.get('/user/:useriD-:bookid', (req, res) => {
+    res.send(req.params);
+});
+
+app.get('/search', (req, res) => {
+    const name = req.query.name;
+    const age = req.query.age;
+    res.send(` search result for Name: ${name}, Age: ${age}`);
 });
